6fbc51e95069d20e24bdf898b78d524a052899f5
Amin <amin.fauad@student.aiu.edu.my>
2024-12-19 05:19:42 +0000

    added some stuff

--- readanalog.py
+++ readanalog.py
@@ -9,3 +9,9 @@ BUS = smbus2.SMBus(1)
 
 def readAnalog(pin):
 	return (BUS.read_i2c_block_data(0x48,pin,2)[-1] )
+
+def getAnalog(pin):
+        return (BUS.read_i2c_block_data(0x48,pin,2)[-1] )
+
+
+
