cc059056a331661e0e970630217866004c40fa4a
Amin <amin.fauad@student.aiu.edu.my>
2024-12-23 08:52:56 +0000

    Added camera settings

--- takepic.py
+++ takepic.py
@@ -18,9 +18,32 @@ width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 print(f"Webcam resolution set to: {width}x{height}")
 
+actual_width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+actual_height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+actual_brightness = cap.get(cv2.CAP_PROP_BRIGHTNESS)
+actual_contrast = cap.get(cv2.CAP_PROP_CONTRAST)
+actual_saturation = cap.get(cv2.CAP_PROP_SATURATION)
+actual_hue = cap.get(cv2.CAP_PROP_HUE)
+actual_gain = cap.get(cv2.CAP_PROP_GAIN)
+actual_exposure = cap.get(cv2.CAP_PROP_EXPOSURE)
+actual_autofocus = cap.get(cv2.CAP_PROP_AUTOFOCUS)
+
+print("Camera Settings Before Attempted Update:")
+print(f" - Resolution     : {actual_width} x {actual_height}")
+print(f" - Brightness     : {actual_brightness}")
+print(f" - Contrast       : {actual_contrast}")
+print(f" - Saturation     : {actual_saturation}")
+print(f" - Hue            : {actual_hue}")
+print(f" - Gain           : {actual_gain}")
+print(f" - Exposure       : {actual_exposure}")
+print(f" - Autofocus      : {actual_autofocus}")
+
+
+
+
 # Wait for 10 seconds
 print("Waiting for 10 seconds before capturing the first image...")
-time.sleep(10)
+time.sleep(1)
 
 # Capture the first frame
 ret, frame = cap.read()
